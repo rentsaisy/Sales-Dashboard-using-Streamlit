6e043392dae1de79a00ce2f4cacced804e5014b4
Currents <currentsaisyah@gmail.com>
2026-02-04 16:58:13 +0700

    Creating a Bar Chart Using Plotly (Retailer by Sales)

--- app.py
+++ app.py
@@ -31,3 +31,9 @@ col3, col4, col5 = st.columns([0.1, 0.45, 0.45])
 with col3:
     box_date = str(datetime.datetime.now().strftime("%d %B %Y"))
     st.write(f"Last updated by:  \n {box_date}")
+    
+with col4:
+    fig = px.bar(df, x = "Retailer", y = "TotalSales", labels={"TotalSales" : "Total Sales {$}"}, 
+                 title = "Total Sales by Retailer", hover_data=["TotalSales"],
+                 template="gridon", height=500)
+    st.plotly_chart(fig, use_container_width=True)
