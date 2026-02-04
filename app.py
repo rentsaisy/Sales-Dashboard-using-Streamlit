d9f489f67c8f74e17c2071911d18e4a819bce9e1
Currents <currentsaisyah@gmail.com>
2026-02-04 16:37:30 +0700

    Setting the page and Dashboard heading

--- app.py
+++ app.py
@@ -4,3 +4,25 @@ import datetime
 from PIL import Image
 import plotly.express as px
 import plotly.graph_objects as go
+
+# Read the excel file data
+df = pd.read_excel("Adidas.xlsx")
+st.set_page_config(layout="wide")
+st.markdown('<style>div.block-container{padding-top:1rem;}</style>', unsafe_allow_html=True)
+image = Image.open('adidas-logo.jpg')
+
+col1, col2 = st.columns([0.1,0.9])
+with col1:
+    st.image(image,width=100)
+    
+html_title = """
+    <style>
+    .title.test{
+    font-weight: bold;
+    padding: 5px;
+    border-radius: 6px    
+    }
+    </style>
+    <center><h1 class="title-test">Adidas Interactive Sales Dashboard</h1></center>"""
+with col2:
+    st.markdown(html_title, unsafe_allow_html=True)
